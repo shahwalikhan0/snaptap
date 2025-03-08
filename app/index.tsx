0ec9e750572b8b855f46b1ea6b2abcd30654822b
Tahakhurram <102379944+Tahakhurram@users.noreply.github.com>
2025-03-08 21:30:33 +0500

    leftmenu-profile,setting button added

--- app/index.tsx
+++ app/index.tsx
@@ -15,13 +15,16 @@ const tempUserData: UserDataType = {
 
 export default function Index() {
   const [menuVisible, setMenuVisible] = useState<boolean>(false);
+  const [selectedItem, setSelectedItem] = useState<string>("home");
   const [userData, setUserData] = useState<UserDataType>(tempUserData);
   const colorScheme = useColorScheme();
 
   const handleShowMenu = () => {
     setMenuVisible(!menuVisible);
   };
-
+  useEffect(() => {
+    console.log("selectedItem", selectedItem);
+  }, [selectedItem]);
   return (
     <SafeAreaView style={styles.container}>
       {/* Fixed Header */}
@@ -32,6 +35,7 @@ export default function Index() {
         isVisible={menuVisible}
         // closeMenu={() => setMenuVisible(false)}
         closeMenu={handleShowMenu}
+        setSelectedItem={setSelectedItem}
       />
     </SafeAreaView>
   );
